7bfb377d860f0b63be383f28bb110b8844aeffd4
Fernando Gortari <fgortari@mobdev.cl>
2023-02-21 21:12:24 -0300

    show images on app

---
+++ __tests__/ui/components/select/select.test.jsx
@@ -0,0 +1,5 @@
+import { describe } from "vitest";
+
+describe("given the Select component", () => {
+  it("", () => {});
+});
--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       setBreeds(res.message);
     });
 
-    getImagesByBreed().then((res) => {
+    getImagesByBreed("beagle").then((res) => {
       setBreedImages(res.message);
     });
   }, [getAllBreedsTryCatch, getImagesByBreed]);
@@ -20,6 +20,21 @@ function App() {
   return (
     <>
       <h1>Dog Ceo App</h1>
+      {
+        <div>
+          {breedImages &&
+            breedImages.map((element, index) => {
+              return (
+                <img
+                  key={index}
+                  className="dog__image"
+                  src={element}
+                  alt="perros"
+                />
+              );
+            })}
+        </div>
+      }
     </>
   );
 }
