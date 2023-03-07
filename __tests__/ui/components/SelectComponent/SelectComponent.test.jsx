cfea58a42d3c1cfaab1401cf09af09ffa6cca088
Fernando Gortari <fernando.gortari@gmail.com>
2023-03-07 12:47:46 -0300

    Select component first test done

--- __tests__/ui/components/SelectComponent/SelectComponent.test.jsx
+++ __tests__/ui/components/SelectComponent/SelectComponent.test.jsx
@@ -1,11 +1,18 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { describe, it } from "vitest";
 import { SelectComponent } from "../../../../src/ui/components/SelectComponent/SelectComponent";
 
-describe("given the <SelectComponent /> component", () => {
-  it("should render", () => {
-    render(<SelectComponent />);
-  });
+const options = [
+  { value: "Hound", label: "Hound" },
+  { value: "Beagle", label: "Beagle" },
+  { value: "Akita", label: "Akita" },
+  { value: "Bulldog", label: "Bulldog" },
+];
 
-  
+describe("given the <SelectComponent />", () => {
+  it("should render", () => {
+    render(<SelectComponent breedsOptions={[]} />);
+    screen.debug();
+    // const selectBreed = screen.queryByTestId("select-breed");
+  });
 });
--- src/ui/components/SelectComponent/SelectComponent.jsx
+++ src/ui/components/SelectComponent/SelectComponent.jsx
@@ -14,14 +14,30 @@ export const SelectComponent = ({
   return (
     <>
       <p className="message">Breed:</p>
-      <Select
-        dataTestId="select-breed"
-        className="select"
-        options={breedsOptions}
-        onChange={handleChange}
-      />
+      {breedsOptions.length > 0 ? (
+        <select className="breed__select" data-testid="select-breed">
+          <option value="Select breed" disabled>
+            Select breed
+          </option>
+          {breedsOptions.map((breed) => (
+            <option value={breed.value} key={breed.label}>
+              {breed.value}
+            </option>
+          ))}
+        </select>
+      ) : (
+        <select
+          defaultValue="default"
+          placeholder="default"
+          data-testid="select-breed"
+          className="breed__select"
+        >
+          <option value="default" disabled>Select breed</option>
+          <option disabled>No hay razas para mostrar</option>
+        </select>
+      )}
       <p className="message">Sub-breed:</p>
-      <Select className="select" options={subBreedOptions} />
+      {/* <Select className="select" options={subBreedOptions} /> */}
     </>
   );
 };
